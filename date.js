b7919d343b5d73576c8d0a1f0118bd8e2566cc03
Emmanuel Amiteye <Emmanuel Amiteye>
2022-10-18 22:55:47 +0100

    first-commit

---
+++ date.js
@@ -0,0 +1,47 @@
+exports.getDate = function() {
+
+    const today = new Date();
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    return today.toLocaleString("en-US", options);
+
+}
+
+exports.getDay = function() {
+    const date = new Date().getHours()
+
+    const opt = {
+        day: date < 12 ? 'Good Morning' : date < 18 ? 'Good Afternoon' : 'Good Night'
+    }
+    return JSON.stringify(opt.day);
+    // console.log(opt.day);
+
+}
+
+
+// exports.getHours = function() {
+
+//     const date = new Date().getHours();
+
+//     const option = {
+//         hour: "numeric",
+//         time: "long"
+//     }
+
+// return date.toLocaleString("en-US", option);
+// }
+
+// // exports.getUTCDay = function() {
+
+// // //     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// // //     const d = new Date();
+// // //     let day = weekday[d.getUTCDay()];
+
+// // //     return d.toLocaleDateString("en-Us", day);
+// }
